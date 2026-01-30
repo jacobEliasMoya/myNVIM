655d1c662751a8a0598d27c133adffa0d56ff85a
jacob moya <jemoya1994@Gmail.com>
2026-01-30 01:20:48 -0800

    working on updating and adding formatter, amazing btw lol

--- init.lua
+++ init.lua
@@ -9,150 +9,153 @@ vim.opt.rtp:prepend(lazypath)
 vim.env.CC = "gcc"
 require("lazy").setup({
     {
-        "nvim-tree/nvim-tree.lua",
-        dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function()
-            require("nvim-tree").setup({
-                sync_root_with_cwd = true,
-                respect_buf_cwd = true,
-                update_focused_file = {
-                    enable = true,
-                    update_root = true -- will change tree root to match opened file
-                }
-            })
-        end
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        opts = { format_on_save = { lsp_fallback = true }, }
     }, {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "tokyonight",
-                    section_separators = { left = "", right = "" },
-                    component_separators = { left = "", right = "" },
-                },
-            })
-        end,
-    },{
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
-        lazy = false,
-        build = ":TSUpdate",
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = {
-                    "lua", "typescript", "javascript", "tsx", "html", "css",
-                    "json", "markdown", "markdown_inline"
-                },
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false
-                },
-                indent = {enable = true}
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("nvim-tree").setup({
+            sync_root_with_cwd = true,
+            respect_buf_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = true     -- will change tree root to match opened file
             }
-        end
-    }, {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                integrations = {
-                    cmp = true,
-                    treesitter = true,
-                    telescope = true,
-                    mason = true,
-                    native_lsp = {enabled = true}
-                }
-            })
+        })
+    end
+}, {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "tokyonight",
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+            },
+        })
+    end,
+}, {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require 'nvim-treesitter.configs'.setup {
+            ensure_installed = {
+                "lua", "typescript", "javascript", "tsx", "html", "css",
+                "json", "markdown", "markdown_inline"
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false
+            },
+            indent = { enable = true }
+        }
+    end
+}, {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha",
+            integrations = {
+                cmp = true,
+                treesitter = true,
+                telescope = true,
+                mason = true,
+                native_lsp = { enabled = true }
+            }
+        })
+    end
+}, {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        style = "storm",
+        transparent = true,
+    },
+}, {
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+    end,
+}, {
+    "folke/tokyonight.nvim",
+    lazy = false,        -- load immediately
+    priority = 1000,     -- load before other UI plugins
+    opts = {},           -- safe default
+}, {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline", "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets"
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        require("luasnip.loaders.from_vscode").lazy_load()
 
-        end
-    },{
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            style = "storm",
-            transparent = true,
-        },
-    }  ,{
-        "Tsuzat/NeoSolarized.nvim",
-        lazy = true,
-        priority = 1000,
-        config = function()
-        end,
-    },{
-        "folke/tokyonight.nvim",
-        lazy = false, -- load immediately
-        priority = 1000, -- load before other UI plugins
-        opts = {}, -- safe default
-    },{
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline", "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets"
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({select = true})
-                }),
-                sources = cmp.config.sources({
-                    {name = "nvim_lsp"}, {name = "luasnip"}, {name = "buffer"},
-                    {name = "path"}
-                })
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true })
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" }, { name = "luasnip" }, { name = "buffer" },
+                { name = "path" }
             })
+        })
 
-            -- Cmdline completion
-            cmp.setup.cmdline({"/", "?"}, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {{name = "buffer"}}
-            })
+        -- Cmdline completion
+        cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { { name = "buffer" } }
+        })
 
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({{name = "path"}},
-                    {{name = "cmdline"}})
-            })
-        end
-    }, -- mason
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({ { name = "path" } },
+                { { name = "cmdline" } })
+        })
+    end
+},     -- mason
     {
         "williamboman/mason.nvim",
         config = function() require("mason").setup() end
     }, {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = {"williamboman/mason.nvim"},
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", "ts_ls", "eslint", "html", "cssls", "tailwindcss",
-                    "jsonls"
-                }
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls", "ts_ls", "eslint", "html", "cssls", "tailwindcss",
+                "jsonls"
+            }
 
-            })
-        end
-    }, -- telescope
+        })
+    end
+},     -- telescope
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = {"nvim-lua/plenary.nvim"}, -- Required by Telescope
+        dependencies = { "nvim-lua/plenary.nvim" }, -- Required by Telescope
         config = function() require("telescope").setup() end
-    }, -- lsp support
+    },                                            -- lsp support
     {
         "neovim/nvim-lspconfig",
         config = function()
@@ -160,7 +163,7 @@ require("lazy").setup({
 
             local servers = {
                 lua_ls = {
-                    settings = {Lua = {diagnostics = {globals = {"vim"}}}}
+                    settings = { Lua = { diagnostics = { globals = { "vim" } } } }
                 },
                 ts_ls = {},
                 eslint = {},
@@ -175,18 +178,18 @@ require("lazy").setup({
                 vim.lsp.config(name, vim.tbl_extend("force", {
                     capabilities = capabilities
                 }, config))
-                vim.lsp.enable({name})
+                vim.lsp.enable({ name })
             end
         end
     }
 
 }, {
-        -- options
-        rocks = {enabled = true, hererocks = false}
-    })
+    -- options
+    rocks = { enabled = true, hererocks = false }
+})
 
-vim.g.mapleader = " " 
-vim.g.maplocalleader = " " 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -200,9 +203,8 @@ vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {desc = "Find Files"})
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {desc = "Find Text"})
-vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", {desc = "Toggle treesitter"})
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Find Text" })
+vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle treesitter" })
 
 vim.cmd.colorscheme("tokyonight")
-
