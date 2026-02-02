dc8a3f33a04c56a7d0c1960790bd680600fd86d1
Jacob Moya <jacob.moya@inenvi.com>
2026-02-02 13:42:58 -0800

    updates, adding surround plugin

--- init.lua
+++ init.lua
@@ -54,8 +54,17 @@ require("lazy").setup({
         },
       })
     end,
-  }
-  , {
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  }, {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
